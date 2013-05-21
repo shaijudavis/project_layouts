89295a9b2340162dc5ed29f978cfdc0c094bfccc
Shaiju Davis <shaiu.davis@vanillanetworks.com>
2013-05-21 11:48:55 +0530

    Added view files for the flatpage plus module

---
+++ flatpage/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('flatpages_plus.views',
+    # url(r'^(?P<url>.*)$',
+    #     view='flatpage',
+    #     name='flatpage'
+    # ),
+    (r'^list/', 'list'),
+    (r'^(?P<url>.*)$', 'flatpage'),
+)
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='views',
+    version='0.1.1',
+    description='Separating views file from app',
+    author='shaiju',
+    author_email='shaijudavis@gmail.com',
+    url='',
+    license='MIT',
+    packages=find_packages(),
+)
