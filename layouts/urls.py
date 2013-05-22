2d352fa7ed348da38dddf92fa1bec2682296ea4c
Shaiju Davis <shaiu.davis@vanillanetworks.com>
2013-05-22 16:50:57 +0530

    renamed flatpage to layouts

---
+++ layouts/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('layouts.views',
+    # url(r'^(?P<url>.*)$',
+    #     view='flatpage',
+    #     name='flatpage'
+    # ),
+    (r'^list/', 'list'),
+    (r'^update/(?P<id>\d+)/$', 'update'),
+    (r'^(?P<url>.*)$', 'flatpage'),
+    #(r'^(?P<url>.*)$', 'showpage'),
+)
