6429894145f1dcf85ac2a3db38f0895687da5d41
Shaiju Davis <shaiu.davis@vanillanetworks.com>
2013-05-22 16:50:19 +0530

    renamed flatpage to layouts

--- flatpage/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-
-from flatpages_plus.models import FlatPage
-
-class FlatpageForm(ModelForm):
-    class Meta:
-        model = FlatPage
-        fields = ['url', 'name', 'title', 'content']
