3a6f9eb2d3f8e30737628b1a3b257b55cba21d1b
Shaiju Davis <shaiu.davis@vanillanetworks.com>
2013-05-23 13:59:35 +0530

    Update setup file to add git url

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='views',
+    name='project_layouts',
     version='0.1.1',
     description='Separating views file from app',
     author='shaiju',
     author_email='shaijudavis@gmail.com',
-    url='',
+    url='https://github.com/shaijudavis/project_layouts/blob/master/setup.py',
     license='MIT',
     packages=find_packages(),
 )
